f1c6b7f8c47b6a963662aeb5ca81bfee5512f404
Blake Matis <14blake1994@gmail.com>
2018-08-14 03:01:02 -0700

    Update FileHandler for better file handling
    -Added ability for read method to use the scanner.nextLine

--- src/main/java/enerdapt/io/IFileReader.java
+++ src/main/java/enerdapt/io/IFileReader.java
@@ -2,17 +2,18 @@ package enerdapt.io;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.OutputStream;
 import java.util.Scanner;
 
 public interface IFileReader {
 
     public abstract void read();
 
-    public abstract void write();
+    public abstract void write(OutputStream outputStream);
 
-    public abstract void setScanner(File file);
+    public abstract void setScanner(File file) throws  FileNotFoundException;
 
-    public abstract File getFile(String filePath);
+    public abstract void setFile(String filePath) throws FileNotFoundException;
 
     public abstract void closeFile();
 
