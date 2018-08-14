51d84a44b9bff070cdf8ac4974e5b108ed80e3c4
Blake Matis <14blake1994@gmail.com>
2018-08-14 02:33:43 -0700

    Updated MainFrame to include a Label

--- src/main/java/enerdapt/gui/MainFrame.java
+++ src/main/java/enerdapt/gui/MainFrame.java
@@ -13,10 +13,23 @@ public class MainFrame extends JFrame {
      * Constructor set the title, dimensions,
      * layout, and visibility of the JFrame.
      */
+    private Label filePathLabel;
+
     public MainFrame(){
         super("EnerDapt HTML Builder");
         this.setSize(800, 1000);
+
         this.setLayout(new BorderLayout());
+
+        setCenterComponents();
+
         this.setVisible(true);
     }
+
+    public void setCenterComponents(){
+        filePathLabel = new Label("Enter the HTML file path", this);
+        this.add(filePathLabel, BorderLayout.CENTER);
+
+    }
+
 }
