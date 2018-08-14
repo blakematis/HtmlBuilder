cf148871702ef5384c4f29eb155827b9c95a73b9
Blake Matis <14blake1994@gmail.com>
2018-08-14 03:02:16 -0700

    Update FileHandler for
    skeleton write method added
    Added File not found execeptions including logger info and warning.

--- src/main/java/enerdapt/io/FileHandler.java
+++ src/main/java/enerdapt/io/FileHandler.java
@@ -2,7 +2,10 @@ package enerdapt.io;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 
 /**
@@ -12,18 +15,40 @@ import java.util.Scanner;
 public class FileHandler implements IFileReader{
     private Scanner scanner;
     private String filePath;
+    private File file;
+    private Logger log = Logger.getLogger(getClass().getName());
 
     public FileHandler(String filePath){
         this.filePath = filePath;
+        try{
+            setFile(filePath);
+            setScanner(file);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void read() {
-
+        if(file.canRead()){
+            log.info("Attempting to read file");
+            while(this.scanner.hasNext()){
+                String currLine = scanner.nextLine();
+                log.info(currLine);
+            }
+        }else{
+            log.warning(file.getName() + " does not have proper read permissions");
+        }
     }
 
     @Override
-    public void write() {
+    public void write(OutputStream outputStream) {
+        if(file.canWrite()){
+            log.info("Attmepting to write to file");
+            //TODO write to file.
+        }else{
+            log.warning(file.getName() + "does not have proper write permissions");
+        }
 
     }
 
@@ -32,22 +57,17 @@ public class FileHandler implements IFileReader{
      * @param file
      */
     @Override
-    public void setScanner(File file){
-        try{
-            this.scanner = new Scanner(file);
-        }catch (FileNotFoundException e){
-            throw new NullPointerException(e.getMessage());
-        }
+    public void setScanner(File file)throws FileNotFoundException{
+        this.scanner = new Scanner(file);
+    }
+
+    public File getFile(){
+        return file;
     }
 
     @Override
-    public File getFile(String filePath)
-    {
-        File file = new File(filePath);
-        if(file.exists()){
-            return file;
-        }
-        return null;
+    public void setFile(String filePath)throws FileNotFoundException{
+        this.file = new File(filePath);
     }
 
     @Override
