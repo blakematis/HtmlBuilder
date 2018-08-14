6e4699e49d122df88cf9201c930231b4fdddb5a4
Blake Matis <14blake1994@gmail.com>
2018-08-14 02:33:12 -0700

    Added a custom BeveledBorder extends BevelBorder

---
+++ src/main/java/enerdapt/gui/BeveledBorder.java
@@ -0,0 +1,10 @@
+package enerdapt.gui;
+
+import javax.swing.border.BevelBorder;
+
+public class BeveledBorder extends BevelBorder {
+
+    public BeveledBorder(){
+        super(BevelBorder.LOWERED);
+    }
+}
