1d5996d9a82016e33824702d565dfc44596ea29c
blakematis <14blake1994@gmail.com>
2018-08-16 16:55:35 -0700

    Added Button
    Allows us to add a button to a panel

---
+++ src/main/java/enerdapt/gui/Button.java
@@ -0,0 +1,13 @@
+package enerdapt.gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Button extends JButton {
+
+    public Button(String status) {
+        super(status);
+    }
+
+}
