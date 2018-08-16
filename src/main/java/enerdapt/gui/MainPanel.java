93ba7992d59e8314e2735c76e6b7835530216a73
blakematis <14blake1994@gmail.com>
2018-08-16 16:58:41 -0700

    Added button and fileChooser to mainPanel.
    -Added an actionListener for when the button is pressed to show the fileChooser
    
    -Added an actionListener for the fileChooser to load a file to the fileChooser and change label  to the file path that was loaded. It also changes the FileHandler to get the selected file or to instead be set to null since no file was selected. Changes button to visible

--- src/main/java/enerdapt/gui/MainPanel.java
+++ src/main/java/enerdapt/gui/MainPanel.java
@@ -1,27 +1,87 @@
 package enerdapt.gui;
 
+import enerdapt.io.FileHandler;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainPanel extends JPanel {
 
     private Label filePathLabel;
+    private Button button;
     private TextBox filePathTextBox;
     private FileChooser fileChooser;
+    private FileHandler fileHandler;
 
     public MainPanel() {
-        this.setLayout(new BorderLayout());
+        this.setLayout(new FlowLayout());
         setCenterComponents();
     }
 
     public void setCenterComponents() {
-        filePathLabel = new Label("Enter the HTML file path", this);
-        filePathTextBox = new TextBox("");
+        filePathLabel = new Label("No File Loaded", this);
         fileChooser = new FileChooser();
+        button = new Button("Open File Explorer");
+        fileHandler = null;
+        this.add(filePathLabel);
+        this.add(button);
+        this.add(fileChooser);
+        fileChooser.setVisible(false);
 
-        this.add(filePathLabel, BorderLayout.NORTH);
-        this.add(fileChooser, BorderLayout.CENTER);
-        this.add(filePathTextBox, BorderLayout.SOUTH);
+        //If button is pressed it will open the fileChooser
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fileChooser.setVisible(true);
+                button.setVisible(false);
+            }
+        });
 
+        fileChooser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(fileChooser.isVisible()){
+                    if(fileChooser.getSelectedFile() != null){
+                        fileHandler = new FileHandler(fileChooser.getSelectedFile().getPath());
+                        filePathLabel.setText("File Found: " +fileHandler.getFile().getPath());
+                        fileChooser.setVisible(false);
+                        button.setVisible(true);
+                    }else{
+                        fileHandler = null;
+                        filePathLabel.setText("No File Loaded");
+                        fileChooser.setVisible(false);
+                        button.setVisible(true);
+                    }
+                }
+            }
+        });
+    }
+
+
+    //accessors
+    public Label getFilePathLabel(){
+        return filePathLabel;
+    }
+
+    public void setFilePathLabel(Label filePathLabel){
+        this.filePathLabel = filePathLabel;
+    }
+
+    public Button getButton(){
+        return button;
+    }
+
+    public void setButton(Button button){
+        this.button = button;
+    }
+
+    public FileChooser getFileChooser() {
+        return fileChooser;
+    }
+
+    public void setFileChooser(FileChooser fileChooser){
+        this.fileChooser = fileChooser;
     }
 }
