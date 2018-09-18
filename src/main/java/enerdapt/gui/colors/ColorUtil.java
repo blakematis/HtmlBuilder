69ff00a9a154bf1fbabe8980407382f5874f4982
blakematis <14blake1994@gmail.com>
2018-09-18 13:32:15 -0700

    Added ColorUtil class for handling all color applications.

---
+++ src/main/java/enerdapt/gui/colors/ColorUtil.java
@@ -0,0 +1,35 @@
+package enerdapt.gui.colors;
+
+import java.awt.*;
+
+/**
+ * Class for getting customized colors used throughout this entire application.
+ * This keeps all colors uniformed throughout and styling simpler.
+ */
+public class ColorUtil {
+
+
+    public ColorUtil(){
+
+    }
+
+    /**
+     *
+     * @return EnerDapt Green Style color
+     */
+    public static Color AWTcolorGreen(){
+        //TODO figure out rgb value
+        return new Color(0);
+    }
+
+    /**
+     *
+     * @return Black color.
+     */
+    public static Color AWTcolorBlack(){
+        //TODO figure out rgb value
+        return new Color(0);
+    }
+
+
+}
